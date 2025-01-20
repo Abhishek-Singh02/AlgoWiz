af3eb7ac4673282df89447d90b0013b1e18ba749
Abhishek-Singh <abhishek9502as@gmail.com>
2025-01-20 15:37:30 +0530

    wip: add initial constants

---
+++ src/constants/index.ts
@@ -0,0 +1,22 @@
+import { TileType } from "@types";
+
+export const MAX_ROWS = 20;
+export const MAX_COLS = 20;
+
+export const START_TILE: TileType = {
+    row: 1,
+    col: 1,
+    isStart: true,
+    isWall: false,
+    isEnd: false,
+    distance:Infinity
+};
+
+export const END_TILE: TileType = {
+    row: MAX_ROWS - 2,
+    col: MAX_COLS - 2,
+    isStart: false,
+    isWall: false,
+    isEnd: true,
+    distance:Infinity
+};
